1a72057df72497454f8c8856abff0db17b114023
gouravpatel1996 <patelgouravsuresh@gmail.com>
2017-04-08 14:40:22 +0530

    Add files via upload
    dedicated to python workshop

---
+++ hw1.py
@@ -0,0 +1,39 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import random
+numtopick=["patelgourav@gmail.com","rakeshrock@gmail.com","rajeev@yahoo.com","nischal@outlook.com","prassana@rediff.com"]
+print(random.choice(numtopick))
+
+
+# In[2]:
+
+print(random.choice(numtopick))
+
+
+# In[3]:
+
+print(random.choice(numtopick))
+
+
+# In[4]:
+
+print(random.choice(numtopick))
+
+
+# In[5]:
+
+print(random.choice(numtopick))
+
+
+# In[6]:
+
+print(random.choice(numtopick))
+
+
+# In[ ]:
+
+
+
