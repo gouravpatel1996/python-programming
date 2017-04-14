4561dfc9c8f07b9dd80ac1322d184ee971363113
gouravpatel1996 <patelgouravsuresh@gmail.com>
2017-04-14 13:44:45 +0530

    1.py

---
+++ 1.py
@@ -0,0 +1,6 @@
+import random
+def pick(maillist,numtopick):
+maillist=["patelgourav@gmail.com","rakeshrock@gmail.com","rajeev@yahoo.com","nischal@outlook.com"]
+numtopick=[1,2,3,4,5]
+print("enter numtopick",numtopick)
+print(random.choice(maillist))
